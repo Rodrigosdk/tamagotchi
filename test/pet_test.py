f3fdd85fd1cf169a2c47486d324b4386fea960e3
Rodrigo Mamede <61851910+Rodrigosdk@users.noreply.github.com>
2022-04-29 16:04:26 -0300

    add documentation teste

--- test/pet_test.py
+++ test/pet_test.py
@@ -12,7 +12,7 @@ def test_must_return_age_0():
 
 def test_must_not_return_age_0():
     '''
-    Vai testar verifica se a idade está 
+    Vai testar  se a idade está 
     conseguio ser alterada
     '''
     tamagotchi.age = 10
@@ -22,35 +22,60 @@ def test_must_not_return_age_0():
     assert value_age != 0
 
 def test_must_return_life_100():
+    '''
+    vai testa se tamagotchi.life retorna
+    o valor da vida
+    '''
     value_life = tamagotchi.life
     
     assert value_life == 100
 
 def test_must_not_return_life_100():
+    '''
+    vai testa se tamagotchi.life consegue
+    modificar o valor da vida
+    '''
     tamagotchi.life = 10
 
     value_life = tamagotchi.life
 
     assert value_life != 100
 
-def test_must_return_hungry_100():    
+def test_must_return_hungry_100():
+    '''
+    vai testa se tamagotchi.hungry retorna
+    o valor da fome
+    '''
     value_hungry = tamagotchi.hungry
     
     assert value_hungry == 100
 
 def test_must_not_return_hungry_100():
+    '''
+    vai testa se tamagotchi.hungry consegue 
+    retorna modificar o valor da fome
+    '''
     tamagotchi.hungry = 10
 
     value_hungry = tamagotchi.hungry
 
     assert value_hungry != 100
 
-def test_must_return_power_100():    
+def test_must_return_power_100():
+    '''
+    vai testa se tamagotchi.power retorna
+    o valor da energia 
+    '''
     value_power = tamagotchi.power
     
     assert value_power == 100
 
 def test_must_not_return_power_100():
+    '''
+    vai testa se tamagotchi.power modficar 
+    o valor da energia 
+    '''
+
     tamagotchi.power = 10
 
     value_power = tamagotchi.power
